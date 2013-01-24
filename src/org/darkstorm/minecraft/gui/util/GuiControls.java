5a9204e054b90c09e59925e626aa8573c9d70ac9
DarkStorm <darkstorm@evilminecraft.net>
2013-01-23 20:37:56 -0500

    Include a GuiScreen implementation that will render a GuiManager.

---
+++ src/org/darkstorm/minecraft/gui/util/GuiControls.java
@@ -0,0 +1,110 @@
+package org.darkstorm.minecraft.gui.util;
+
+import java.awt.Rectangle;
+
+import net.minecraft.src.GuiScreen;
+
+import org.darkstorm.minecraft.gui.GuiManager;
+import org.darkstorm.minecraft.gui.component.*;
+
+public class GuiControls extends GuiScreen {
+	private final GuiManager guiManager;
+
+	public GuiControls(GuiManager guiManager) {
+		this.guiManager = guiManager;
+	}
+
+	@Override
+	protected void mouseClicked(int x, int y, int button) {
+		super.mouseClicked(x, y, button);
+		for(Frame frame : guiManager.getFrames()) {
+			if(!frame.isVisible())
+				continue;
+			if(!frame.isMinimized() && !frame.getArea().contains(x, y)) {
+				for(Component component : frame.getChildren()) {
+					for(Rectangle area : component.getTheme()
+							.getUIForComponent(component)
+							.getInteractableRegions(component)) {
+						if(area.contains(x - frame.getX() - component.getX(), y
+								- frame.getY() - component.getY())) {
+							frame.onMousePress(x - frame.getX(),
+									y - frame.getY(), button);
+							guiManager.bringForward(frame);
+							return;
+						}
+					}
+				}
+			}
+		}
+		for(Frame frame : guiManager.getFrames()) {
+			if(!frame.isVisible())
+				continue;
+			if(!frame.isMinimized() && frame.getArea().contains(x, y)) {
+				frame.onMousePress(x - frame.getX(), y - frame.getY(), button);
+				guiManager.bringForward(frame);
+				break;
+			} else if(frame.isMinimized()) {
+				for(Rectangle area : frame.getTheme().getUIForComponent(frame)
+						.getInteractableRegions(frame)) {
+					if(area.contains(x - frame.getX(), y - frame.getY())) {
+						frame.onMousePress(x - frame.getX(), y - frame.getY(),
+								button);
+						guiManager.bringForward(frame);
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void mouseMovedOrUp(int x, int y, int button) {
+		super.mouseMovedOrUp(x, y, button);
+		for(Frame frame : guiManager.getFrames()) {
+			if(!frame.isVisible())
+				continue;
+			if(!frame.isMinimized() && !frame.getArea().contains(x, y)) {
+				for(Component component : frame.getChildren()) {
+					for(Rectangle area : component.getTheme()
+							.getUIForComponent(component)
+							.getInteractableRegions(component)) {
+						if(area.contains(x - frame.getX() - component.getX(), y
+								- frame.getY() - component.getY())) {
+							frame.onMouseRelease(x - frame.getX(),
+									y - frame.getY(), button);
+							guiManager.bringForward(frame);
+							return;
+						}
+					}
+				}
+			}
+		}
+		for(Frame frame : guiManager.getFrames()) {
+			if(!frame.isVisible())
+				continue;
+			if(!frame.isMinimized() && frame.getArea().contains(x, y)) {
+				frame.onMouseRelease(x - frame.getX(), y - frame.getY(), button);
+				guiManager.bringForward(frame);
+				break;
+			} else if(frame.isMinimized()) {
+				for(Rectangle area : frame.getTheme().getUIForComponent(frame)
+						.getInteractableRegions(frame)) {
+					if(area.contains(x - frame.getX(), y - frame.getY())) {
+						frame.onMouseRelease(x - frame.getX(),
+								y - frame.getY(), button);
+						guiManager.bringForward(frame);
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void drawScreen(int par2, int par3, float par4) {
+		Frame[] frames = guiManager.getFrames();
+		for(int i = frames.length - 1; i >= 0; i--)
+			frames[i].render();
+		super.drawScreen(par2, par3, par4);
+	}
+}
